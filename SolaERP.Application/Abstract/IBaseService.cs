1e6f9819c89347fde4ca9a53392013f433db911e
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2022-11-18 21:06:01 +0400

    Implementation of Service Layer

---
+++ SolaERP.Application/Abstract/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using SolaERP.Infrastructure.Dtos;
+
+namespace SolaERP.Application.Abstract
+{
+    public interface IBaseService<T>
+    {
+        ApiResponse<List<T>> GetAll();
+        ApiResponse<T> Register(T model);
+    }
+}
---
+++ SolaERP.Application/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using SolaERP.Application.Abstract;
+using SolaERP.DataAccess.Abstract;
+using SolaERP.Infrastructure.Dtos;
+
+namespace SolaERP.Application.Services
+{
+    public class UserService : IBaseService<UserDto>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public UserService(IUserRepository userRepository, IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public ApiResponse<UserDto> Register(UserDto model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApiResponse<List<UserDto>> GetAll()
+        {
+            var users = _userRepository.GetAllAsync();
+            var dto = _mapper.Map<List<UserDto>>(users);
+
+            return ApiResponse<List<UserDto>>.Success(dto, 200);
+        }
+    }
+}
