5b3a2f838402ba2414df3afb3d48ac01cd4c3ef0
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2022-11-18 21:05:06 +0400

    Implementation of AutoMApper

---
+++ SolaERP.Application/Mappers/MapProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SolaERP.Infrastructure.Dtos;
+using SolaERP.Infrastructure.Entities.Auth;
+
+namespace SolaERP.Application.Mappers
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+        }
+    }
+}
