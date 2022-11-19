17fa18b3d8c64acfc7f7236a11420f75bc734838
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2022-11-19 22:32:53 +0400

    Fixing Infrastructure

--- SolaERP.Infrastructure/Services/IUserService.cs
+++ SolaERP.Infrastructure/Services/IUserService.cs
@@ -1,8 +1,10 @@
 ﻿using SolaERP.Infrastructure.Dtos;
+using SolaERP.Infrastructure.Dtos.Auth;
 
 namespace SolaERP.Infrastructure.Services
 {
     public interface IUserService : IBaseService<UserDto>
     {
+        Task<ApiResponse<Token>> LoginAsync(LoginRequestDto user);
     }
 }
