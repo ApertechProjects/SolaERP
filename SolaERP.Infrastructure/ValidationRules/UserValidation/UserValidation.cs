1d9a6cd3b1912e8f5f4eff6d34d0b8a7d73eacad
hulya.garibli <qeriblih@gmail.com>
2022-11-23 00:57:53 +0400

    UserValidation Filter Added

---
+++ SolaERP.Infrastructure/ValidationRules/UserValidation/UserValidation.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using SolaERP.Infrastructure.Dtos.UserDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolaERP.Infrastructure.ValidationRules.UserValidation
+{
+    public class UserValidation : AbstractValidator<UserDto>
+    {
+        public UserValidation()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Please, enter {PropertyName}");
+
+            RuleFor(x => x.FullName).NotEmpty().WithMessage("Please, enter {PropertyName}");
+
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Please, enter {PropertyName}");
+
+            RuleFor(x => x.PasswordHash).NotEmpty().WithMessage("Please, enter {PropertyName}");
+
+            RuleFor(x => x.ConfirmPasswordHash).NotEmpty().WithMessage("Please, enter {PropertyName}");
+        }
+    }
+}
---
+++ SolaERP.Infrastructure/ValidationRules/ValidationFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SolaERP.Infrastructure.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolaERP.Infrastructure.ValidationRules
+{
+    public class ValidationFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var modelState = context.ModelState;
+            var errorText = modelState.Select(x => x.Value.Errors).Where(y => y.Count > 0).ToList()[0][0].ErrorMessage;
+            if (!modelState.IsValid)
+            {
+                context.Result = new ObjectResult(ApiResponse<bool>.Fail(errorText, 400));
+            }
+        }
+    }
+}
