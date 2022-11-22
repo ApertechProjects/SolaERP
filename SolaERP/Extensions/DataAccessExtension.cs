32191585a8b4789c301895f187f2b1cd1d98c7a3
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2022-11-22 12:34:57 +0400

    Fixing

--- SolaERP/Extensions/DataAccessExtension.cs
+++ SolaERP/Extensions/DataAccessExtension.cs
@@ -13,10 +13,10 @@ namespace SolaERP.Extensions
     {
         public static void UseSqlDataAccessServices(this WebApplicationBuilder builder)
         {
-            builder.Services.AddSingleton<IUnitOfWork, SqlUnitOfWork>();
+            builder.Services.AddTransient<IUnitOfWork, SqlUnitOfWork>();
             builder.Services.AddSingleton<UserService, UserService>();
             builder.Services.AddSingleton<IUserRepository, SqlUserRepository>();
-            builder.Services.AddSingleton((t) =>
+            builder.Services.AddTransient((t) =>
             {
                 var connectionString = builder.Configuration.GetConnectionString("DevelopmentConnectionString");
                 return ConnectionFactory.CreateSqlConnection(connectionString);
