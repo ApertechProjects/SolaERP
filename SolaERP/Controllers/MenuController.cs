92eec6c3ebaf185a7a8d2234560a70f51527ac8c
shefashahgaldiyeva <83983156+shefashahgaldiyeva@users.noreply.github.com>
2022-12-06 10:03:14 +0400

    Test

---
+++ SolaERP/Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolaERP.Infrastructure.Contracts.Services;
+using SolaERP.Infrastructure.Dtos.Menu;
+using SolaERP.Infrastructure.Dtos.Shared;
+
+namespace SolaERP.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        private readonly IMenuService _menuService;
+
+        public MenuController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+
+
+        [HttpGet]
+        public async Task<ApiResponse<List<ParentMenuDto>>> GetUserMenusWithChilds()
+        {
+            return await _menuService.GetUserMenusWithChildsAsync();
+        }
+    }
+}
