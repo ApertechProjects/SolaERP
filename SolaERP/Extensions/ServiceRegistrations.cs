a4142d9d765772ab7a303803936bf9bd2f6353e0
shefashahgaldiyeva <83983156+shefashahgaldiyeva@users.noreply.github.com>
2022-12-16 13:24:53 +0400

    Revert "Bug fixed"
    This reverts commit bceb24281ca8741cf65f0b1afe550fceff74c48c.

--- SolaERP/Extensions/ServiceRegistrations.cs
+++ SolaERP/Extensions/ServiceRegistrations.cs
@@ -40,7 +40,6 @@ namespace SolaERP.Extensions
             builder.Services.AddScoped<IApproveRoleService, ApproveRoleService>();
             builder.Services.AddScoped<IProcedureService, ProcedureService>();
             builder.Services.AddScoped<IMailService, MailService>();
-            builder.Services.AddScoped<IChatService, ChatService>();
         }
         private static void UseRepositories(this WebApplicationBuilder builder)
         {
@@ -53,7 +52,6 @@ namespace SolaERP.Extensions
             builder.Services.AddScoped<IApproveStageRoleRepository, SqlApproveStageRoleRepository>();
             builder.Services.AddScoped<IApproveRoleRepository, SqlApproveRoleRepository>();
             builder.Services.AddScoped<IProcedureRepository, SqlProcedureRepository>();
-            builder.Services.AddScoped<IChatRepository, SqlChatRepository>();
         }
         public static void UseValidationExtension(this WebApplicationBuilder builder)
         {
