bee66a4edef809a42bc66d1163cea7bd240474a1
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2022-12-31 13:33:08 +0400

    Vendors Addede

--- SolaERP.Infrastructure/Contracts/Repositories/IVendorRepository.cs
+++ SolaERP.Infrastructure/Contracts/Repositories/IVendorRepository.cs
@@ -2,7 +2,10 @@
 
 namespace SolaERP.Infrastructure.Contracts.Repositories
 {
-    internal interface IVendorRepository : ICrudOperations<Vendors>
+    public interface IVendorRepository : ICrudOperations<Vendor>
     {
+        public Task<List<Vendor>> GetVendorDrafts(int userId, int businessUnitId);
+        public Task<List<Vendor>>GetVendorWFA(int userId, int businessUnitId);  
+
     }
 }
--- SolaERP.Infrastructure/Entities/Vendors/Vendors.cs
+++ SolaERP.Infrastructure/Entities/Vendors/Vendors.cs
@@ -1,6 +1,8 @@
-﻿namespace SolaERP.Infrastructure.Entities.Vendors
+﻿using SolaERP.Infrastructure.Entities.Auth;
+
+namespace SolaERP.Infrastructure.Entities.Vendors
 {
-    public class Vendors : BaseEntity
+    public class Vendor : BaseEntity
     {
         public int VendorId { get; set; }
         public string VendorCode { get; set; }
@@ -15,11 +17,17 @@
         public int V60DaysPayment { get; set; } //db name 60DaysPayment
         public string Country { get; set; }
         public int Status { get; set; }
+        public string StatusName { get; set; }
+        public string ApproveStatusName { get; set; }
         public int BusinessUnitId { get; set; }
         public string OtherProducts { get; set; }
         public int ApproveStageMainId { get; set; }
         public string CompanyAddress { get; set; }
         public DateTime CompanyRegistrationDate { get; set; }
         public string TaxOffice { get; set; }
+        public int UserId { get; set; }
+        public string UserStatusName { get; set; }
+        public VendorApprovals VendorApprovals { get; set; }
+        public User AppUser { get; set; }
     }
 }
