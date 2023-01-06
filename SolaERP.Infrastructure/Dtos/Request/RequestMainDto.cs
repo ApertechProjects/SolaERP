698375611d55bd48a185f9fb3f1f393493206201
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2023-01-06 10:02:38 +0400

    Models

---
+++ SolaERP.Infrastructure/Dtos/Request/RequestMainDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SolaERP.Infrastructure.Dtos.Request
+{
+    public class RequestMainDto
+    {
+        public int RequestMainID { get; set; }
+        public string BusinessUnitCode { get; set; }
+        public int RowNum { get; set; }
+        public string RequestType { get; set; }
+        public string RequestNo { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime RequestDeadline { get; set; }
+    }
+}
