6f05a741249423760c131e5e96bcdd8394c4fb6c
hulya.garibli <qeriblih@gmail.com>
2023-01-10 01:17:05 +0400

    libraries added

--- SolaERP/Program.cs
+++ SolaERP/Program.cs
@@ -1,3 +1,19 @@
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using SolaERP.Application.Mappers;
+using SolaERP.Application.Validations;
+using SolaERP.Business.Models;
+using SolaERP.Extensions;
+using SolaERP.Middlewares;
+using SolaERP.SignalR.Hubs;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(options => { options.Filters.Add(new ValidationFilter()); })
