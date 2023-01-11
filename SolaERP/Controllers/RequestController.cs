29b1c1a56300a872a17b440fbfa9c7043cfe7567
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2023-01-11 16:23:09 +0400

    Fix

--- SolaERP/Controllers/RequestController.cs
+++ SolaERP/Controllers/RequestController.cs
@@ -10,7 +10,6 @@ namespace SolaERP.Controllers
     [ApiController]
     public class RequestController : ControllerBase
     {
-
         private readonly IRequestService _requestService;
 
         public RequestController(IRequestService requestService)
