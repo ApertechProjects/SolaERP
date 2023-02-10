1de4b59aa7f3c72957946b58995ea291fa97aaac
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2023-02-10 17:11:29 +0400

    User models changed

--- SolaERP.Infrastructure/Dtos/User/ResetPasswordRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SolaERP.Infrastructure.Dtos.User
-{
-    public class ResetPasswordRequestDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
-    }
-}
