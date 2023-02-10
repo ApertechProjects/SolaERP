5412f36e58855de96428b7bbede6b277caa8b763
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2023-02-10 17:09:34 +0400

    Request models changed

---
+++ SolaERP.Infrastructure/Dtos/Request/RequestApprovalInfoDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SolaERP.Infrastructure.Dtos.Request
+{
+    public class RequestApprovalInfoDto
+    {
+        public int Sequesnce { get; set; }
+        public DateTime ApproveDate { get; set; }
+        public int UserId { get; set; }
+        public string ApprovedBy { get; set; }
+        public string Comment { get; set; }
+        public int ApproveStatus { get; set; }
+        public string ApprovalStatusName { get; set; }
+    }
+}
---
+++ SolaERP.Infrastructure/Dtos/Request/RequestDetailsWithAnalysisCodeDto.cs
@@ -0,0 +1,37 @@
+﻿namespace SolaERP.Infrastructure.Dtos.Request
+{
+    public class RequestDetailsWithAnalysisCodeDto
+    {
+        public int RequestDetailId { get; set; }
+        public int RequestMainId { get; set; }
+        public int LineNo { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime RequestDeadline { get; set; }
+        public DateTime RequestedDate { get; set; }
+        public int ItemCode { get; set; }
+        public decimal Quantity { get; set; }
+        public string UOM { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string Buyer { get; set; }
+        public decimal AvailableQuantity { get; set; }
+        public decimal QuantityFromStock { get; set; }
+        public decimal OriginalQuantity { get; set; }
+        public decimal TotalBudget { get; set; }
+        public decimal RemainingBudget { get; set; }
+        public decimal Amount { get; set; }
+        public string ConnectedOrderReferance { get; set; }
+        public int ConnectecOrderLineNo { get; set; }
+        public string AccountCode { get; set; }
+        public int RequestAnalysisCode1Id { get; set; }
+        public int RequestAnalysisCode2Id { get; set; }
+        public int RequestAnalysisCode3Id { get; set; }
+        public int RequestAnalysisCode4Id { get; set; }
+        public int RequestAnalysisCode5Id { get; set; }
+        public int RequestAnalysisCode6Id { get; set; }
+        public int RequestAnalysisCode7Id { get; set; }
+        public int RequestAnalysisCode8Id { get; set; }
+        public int RequestAnalysisCode9Id { get; set; }
+        public int RequestAnalysisCode10Id { get; set; }
+    }
+}
