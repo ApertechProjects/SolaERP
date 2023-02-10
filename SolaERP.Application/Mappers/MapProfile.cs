d8024bb51ac262cdb668874b51c369aa1df52433
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2023-02-10 17:17:11 +0400

    namespaces

--- SolaERP.Application/Mappers/MapProfile.cs
+++ SolaERP.Application/Mappers/MapProfile.cs
@@ -1,4 +1,31 @@
-﻿namespace SolaERP.Application.Mappers
+﻿using AutoMapper;
+using SolaERP.Infrastructure.Dtos;
+using SolaERP.Infrastructure.Dtos.AnalysisCode;
+using SolaERP.Infrastructure.Dtos.ApproveRole;
+using SolaERP.Infrastructure.Dtos.ApproveStage;
+using SolaERP.Infrastructure.Dtos.ApproveStages;
+using SolaERP.Infrastructure.Dtos.BusinessUnit;
+using SolaERP.Infrastructure.Dtos.Group;
+using SolaERP.Infrastructure.Dtos.Item_Code;
+using SolaERP.Infrastructure.Dtos.LogInfo;
+using SolaERP.Infrastructure.Dtos.Menu;
+using SolaERP.Infrastructure.Dtos.Procedure;
+using SolaERP.Infrastructure.Dtos.Request;
+using SolaERP.Infrastructure.Dtos.UserDto;
+using SolaERP.Infrastructure.Entities;
+using SolaERP.Infrastructure.Entities.ApproveRole;
+using SolaERP.Infrastructure.Entities.ApproveStage;
+using SolaERP.Infrastructure.Entities.ApproveStages;
+using SolaERP.Infrastructure.Entities.Auth;
+using SolaERP.Infrastructure.Entities.BusinessUnits;
+using SolaERP.Infrastructure.Entities.Groups;
+using SolaERP.Infrastructure.Entities.Item_Code;
+using SolaERP.Infrastructure.Entities.LogInfo;
+using SolaERP.Infrastructure.Entities.Menu;
+using SolaERP.Infrastructure.Entities.Procedure;
+using SolaERP.Infrastructure.Entities.Request;
+
+namespace SolaERP.Application.Mappers
 {
     public class MapProfile : Profile
     {
