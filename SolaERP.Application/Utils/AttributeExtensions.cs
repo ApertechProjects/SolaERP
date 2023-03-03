b8ed4c73d22466877b99ceb332d9666f79577f86
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2023-03-03 17:43:16 +0400

    Layout

---
+++ SolaERP.Application/Utils/AttributeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Extender;
+
+namespace SolaERP.Application.Utils
+{
+    public static class AttributeExtensions
+    {
+        public static void SetAttribuute(this Attribute attribute, string className, string propertyName, Type propertyType, bool isReadonly = false, object[] attributeParams = null)
+        {
+            var attributeType = attribute.GetType();
+
+            var typeExtender = new TypeExtender(className);
+            typeExtender.AddProperty(propertyName, propertyType, attributeType, attributeParams, isReadonly);
+        }
+    }
+}
