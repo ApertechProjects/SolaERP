e4b2971fa00116b9ff11dbb06d95b67908d0c549
Hulya <hulya.garibli@apertech.net>
2023-03-09 16:47:06 +0400

    bug fixed

--- SolaERP.Application/Validations/AttachmentValidation/AttachmentValidation.cs
+++ SolaERP.Application/Validations/AttachmentValidation/AttachmentValidation.cs
@@ -7,12 +7,10 @@ namespace SolaERP.Application.Validations.AttachmentValidation
     {
         public AttachmentValidation()
         {
-            RuleFor(x => x.AttachmentSubTypeId).NotEmpty().WithMessage("{PropertyName}: Please,enter {PropertyName}");
             RuleFor(x => x.SourceId).NotEmpty().WithMessage("{PropertyName}: Please,enter {PropertyName}");
             RuleFor(x => x.FileData).NotEmpty().WithMessage("{PropertyName}: Please,enter {PropertyName}");
             RuleFor(x => x.FileName).NotEmpty().WithMessage("{PropertyName}: Please,enter {PropertyName}");
             RuleFor(x => x.ExtensionType).NotEmpty().WithMessage("{PropertyName}: Please,enter {PropertyName}");
-            RuleFor(x => x.AttachmentTypeId).NotEmpty().WithMessage("{PropertyName}: Please,enter {PropertyName}");
             RuleFor(x => x.SourceType).NotEmpty().WithMessage("{PropertyName}: Please,enter {PropertyName}");
         }
 
