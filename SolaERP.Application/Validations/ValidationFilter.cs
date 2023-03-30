609a526120d8b8a03a81fd116f54772b61225e04
Hulya <hulya.garibli@apertech.net>
2023-03-30 15:53:39 +0400

    comment added

--- SolaERP.Application/Validations/ValidationFilter.cs
+++ SolaERP.Application/Validations/ValidationFilter.cs
@@ -21,22 +21,23 @@ namespace SolaERP.Application.Validations
                .Where(x => x.Value.Errors.Count > 0)
                .Select(x => x.Key.Split(".").Last())
                .ToList();
-
-                //var errorText = modelState.Select(x => x.Value.Errors).Where(y => y.Count > 0).ToList()[0][0].ErrorMessage;
-                //var property = modelState
-                //.Where(x => x.Value.Errors.Count > 0)
-                //.Select(x => x.Key.Split(".").Last())
-                //.ToList()[0];
                 context.Result = new ObjectResult(ApiResponse<bool>.Fail(propert, errorTex, 422));
             }
 
 
-            //var modelState = context.ModelState;
+
+            //var modelState = context.ModelState; //mergebranch
             //if (!modelState.IsValid)
             //{
             //    var errorText = modelState.Select(x => x.Value.Errors).Where(y => y.Count > 0).ToList()[0][0].ErrorMessage;
-            //    context.Result = new ObjectResult(ApiResponse<bool>.Fail(errorText, 400));
+            //    var property = modelState
+            //    .Where(x => x.Value.Errors.Count > 0)
+            //    .Select(x => x.Key.Split(".").Last())
+            //    .ToList()[0];
+            //    context.Result = new ObjectResult(ApiResponse<bool>.Fail(property, errorText, 422));
+
             //}
+
         }
     }
 }
