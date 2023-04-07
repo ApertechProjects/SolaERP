ce53619c2c82afefa8b4fc4bccbf13ab342b6f79
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2023-04-07 11:23:58 +0400

    Tes

--- SolaERP.Infrastructure/Entities/User/UserMain.cs
+++ SolaERP.Infrastructure/Entities/User/UserMain.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string UserPhoto { get; set; }
+        public string UserSignature { get; set; }
         public string FullName { get; set; }
         public string ApproveStatus { get; set; }
         public Int64 RowNum { get; set; }
