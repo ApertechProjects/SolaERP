9a03fa70943e26651eb9c970db2fb82a70b12d0e
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2023-04-12 12:37:34 +0400

    Vendor id

--- SolaERP.Infrastructure/Dtos/Supplier/SupplierCodeDto.cs
+++ SolaERP.Infrastructure/Dtos/Supplier/SupplierCodeDto.cs
@@ -2,6 +2,7 @@
 {
     public class SupplierCodeDto
     {
+        public int SupplierId { get; set; }
         public string SupplierCode { get; set; }
         public string Name { get; set; }
         public string TaxId { get; set; }
--- SolaERP.Infrastructure/Entities/Supplier/SupplierCode.cs
+++ SolaERP.Infrastructure/Entities/Supplier/SupplierCode.cs
@@ -2,6 +2,7 @@
 {
     public class SupplierCode : BaseEntity
     {
+        public int SupplierId { get; set; }
         public string SuppCode { get; set; }
         public string Name { get; set; }
         public string TaxId { get; set; }
