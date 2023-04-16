6579dd5c59b8736f094ff274a4c203c3653126e5
hulya.garibli <qeriblih@gmail.com>
2023-04-16 15:08:42 +0400

    bug fixed

--- SolaERP.Application/Services/UserService.cs
+++ SolaERP.Application/Services/UserService.cs
@@ -354,7 +354,7 @@ namespace SolaERP.Application.Services
             string serverSignaturePath = string.Empty;
 
             if (user.Password != user.ConfirmPassword)
-                return ApiResponse<bool>.Fail("Confirm Password", " Confirm Password doesn't match the Password!", 422);
+                return ApiResponse<bool>.Fail("confirm Password", " Confirm Password doesn't match the Password!", 422);
 
             if (!string.IsNullOrEmpty(user.Password))
                 userEntry.PasswordHash = SecurityUtil.ComputeSha256Hash(user?.Password);
