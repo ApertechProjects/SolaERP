82a345820ea18d6bcc5fb49c9aa91f209ad7296e
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2023-04-26 11:52:08 +0400

    G

--- SolaERP/Controllers/GroupController.cs
+++ SolaERP/Controllers/GroupController.cs
@@ -22,7 +22,9 @@ namespace SolaERP.Controllers
         public async Task<IActionResult> GetGroupsAsync()
             => CreateActionResult(await _groupService.GetAllAsync());
 
-
+        [HttpGet("{groupid}")]
+        public async Task<IActionResult> GetEmailNotifications(int groupid)
+           => CreateActionResult(await _groupService.GetGroupEmailNotificationsAsync(groupid));
 
         [HttpGet]
         public async Task<IActionResult> GetGroupInfoAsync(int groupId)
@@ -36,13 +38,25 @@ namespace SolaERP.Controllers
         public async Task<IActionResult> GetBuyersByGroupIdAsync(int groupId)
            => CreateActionResult(await _groupService.GetBuyersByGroupIdAsync(groupId));
 
+        [HttpGet]
+        public async Task<IActionResult> GetGroupRolesByGroupIdAsync(int groupId)
+            => CreateActionResult(await _groupService.GetGroupRolesByGroupIdAsync(groupId));
+
+        [HttpGet("{groupId}")]
+        public async Task<IActionResult> GetAdditionalPrivilegesByGroup(int groupId)
+          => CreateActionResult(await _groupService.GetAdditionalPrivilegesForGroupAsync(groupId));
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> AvailableGroupsForUser(int userId)
+          => CreateActionResult(await _groupService.GetUserGroupsWithoutCurrents(userId));
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsersByGroupIdAsync([FromQuery] int groupId)
+          => CreateActionResult(await _userService.GetUsersByGroupIdAsync(groupId));
+
         [HttpPost]
         public async Task<IActionResult> SaveBuyerByGroupAsync(GroupBuyerSaveModel model)
             => CreateActionResult(await _groupService.SaveBuyerByGroupAsync(model));
-
-        [HttpDelete("{groupBuyerId}")]
-        public async Task<IActionResult> DeleteBuyerByGroupIdAsync(int groupBuyerId)
-            => CreateActionResult(await _groupService.DeleteBuyerByGroupIdAsync(groupBuyerId));
 
         [HttpGet("{groupId}")]
         public async Task<IActionResult> GetAnalysisCodesByGroupIdAsync(int groupId)
@@ -51,38 +65,6 @@ namespace SolaERP.Controllers
         [HttpPost]
         public async Task<IActionResult> SaveAnalysisCodeByGroupAsync(AnalysisCodeSaveModel model)
             => CreateActionResult(await _groupService.SaveAnalysisCodeByGroupAsync(model));
-
-        [HttpDelete("{groupAnalysisCodeId}")]
-        public async Task<IActionResult> DeleteAnalysisCodeByGroupIdAsync(int groupAnalysisCodeId)
-            => CreateActionResult(await _groupService.DeleteAnalysisCodeByGroupIdAsync(groupAnalysisCodeId));
-
-        [HttpGet]
-        public async Task<IActionResult> GetGroupRolesByGroupIdAsync(int groupId)
-            => CreateActionResult(await _groupService.GetGroupRolesByGroupIdAsync(groupId));
-
-        [HttpPost]
-        public async Task<IActionResult> SaveGroupRoleByGroupAsync(GroupRoleSaveModel model)
-         => CreateActionResult(await _groupService.SaveGroupRoleByGroupAsync(model));
-
-        [HttpDelete("{groupApproveRoleId}")]
-        public async Task<IActionResult> DeleteGroupRoleByGroupIdAsync(int groupApproveRoleId)
-            => CreateActionResult(await _groupService.DeleteGroupRoleByGroupIdAsync(groupApproveRoleId));
-
-        [HttpGet("{groupId}")]
-        public async Task<IActionResult> GetAdditionalPrivilegesByGroup(int groupId)
-            => CreateActionResult(await _groupService.GetAdditionalPrivilegesForGroupAsync(groupId));
-
-        [HttpGet("{userId}")]
-        public async Task<IActionResult> AvailableGroupsForUser(int userId)
-          => CreateActionResult(await _groupService.GetUserGroupsWithoutCurrents(userId));
-
-        [HttpDelete("{groupEmailNotificationId}")]
-        public async Task<IActionResult> DeleteEmailNotification(int groupEmailNotificationId)
-            => CreateActionResult(await _groupService.DeleteEmailNotificationAsync(groupEmailNotificationId));
-
-        [HttpGet("{groupid}")]
-        public async Task<IActionResult> GetEmailNotifications(int groupid)
-            => CreateActionResult(await _groupService.GetGroupEmailNotificationsAsync(groupid));
 
         [HttpPost]
         public async Task<IActionResult> CreateEmailNotification(CreateGroupEmailNotificationModel model)
@@ -96,13 +78,28 @@ namespace SolaERP.Controllers
         public async Task<IActionResult> AddUserToGroupAsync(List<AddUserToGroupModel> model)
             => CreateActionResult(await _groupService.AddUserToGroupAsync(model));
 
+        [HttpPost]
+        public async Task<IActionResult> SaveGroupRoleByGroupAsync(GroupRoleSaveModel model)
+         => CreateActionResult(await _groupService.SaveGroupRoleByGroupAsync(model));
+
+        [HttpDelete("{groupApproveRoleId}")]
+        public async Task<IActionResult> DeleteGroupRoleByGroupIdAsync(int groupApproveRoleId)
+            => CreateActionResult(await _groupService.DeleteGroupRoleByGroupIdAsync(groupApproveRoleId));
+
+        [HttpDelete("{groupEmailNotificationId}")]
+        public async Task<IActionResult> DeleteEmailNotification(int groupEmailNotificationId)
+            => CreateActionResult(await _groupService.DeleteEmailNotificationAsync(groupEmailNotificationId));
+
+        [HttpDelete("{groupBuyerId}")]
+        public async Task<IActionResult> DeleteBuyerByGroupIdAsync(int groupBuyerId)
+          => CreateActionResult(await _groupService.DeleteBuyerByGroupIdAsync(groupBuyerId));
+
+        [HttpDelete("{groupAnalysisCodeId}")]
+        public async Task<IActionResult> DeleteAnalysisCodeByGroupIdAsync(int groupAnalysisCodeId)
+            => CreateActionResult(await _groupService.DeleteAnalysisCodeByGroupIdAsync(groupAnalysisCodeId));
+
         [HttpDelete]
         public async Task<IActionResult> DeleteUserFromGroupAsync(List<int> groupUserIds)
             => CreateActionResult(await _groupService.DeleteUserFromGroupAsync(groupUserIds));
-
-        [HttpGet]
-        public async Task<IActionResult> GetUsersByGroupIdAsync([FromQuery] int groupId)
-           => CreateActionResult(await _userService.GetUsersByGroupIdAsync(groupId));
-
     }
 }
