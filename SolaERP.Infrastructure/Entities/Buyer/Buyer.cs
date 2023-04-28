b322da1c98381fc0eed3ee8cec64c14b64a8658e
Hulya <hulya.garibli@apertech.net>
2023-04-28 17:06:52 +0400

    buyg fixed

--- SolaERP.Infrastructure/Entities/Buyer/Buyer.cs
+++ SolaERP.Infrastructure/Entities/Buyer/Buyer.cs
@@ -2,7 +2,7 @@
 {
     public class Buyer : BaseEntity
     {
-        public string EmployeeCode { get; set; }
-        public string EmployeeName { get; set; }
+        public string BuyerCode { get; set; }
+        public string BuyerName { get; set; }
     }
 }
