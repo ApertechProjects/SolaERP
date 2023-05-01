915c1b900039b9e9160ade5bca0e62ffa728d402
Hulya <hulya.garibli@apertech.net>
2023-05-01 09:01:07 +0400

    validation changed

--- SolaERP.Application/Validations/UserValidation/UserSaveModelValidation.cs
+++ SolaERP.Application/Validations/UserValidation/UserSaveModelValidation.cs
@@ -7,19 +7,13 @@ namespace SolaERP.Persistence.Validations.UserValidation
     {
         public UserSaveModelValidation()
         {
+            RuleFor(x=>x.UserTypeId).NotEmpty().WithMessage("Please, enter {PropertyName}");
             RuleFor(x => x.UserName).NotEmpty().WithMessage("Please, enter {PropertyName}").MinimumLength(3);
             RuleFor(x => x.FullName).NotEmpty().WithMessage("Please, enter {PropertyName}");
             RuleFor(x => x.Email).NotEmpty().WithMessage("Please, enter {PropertyName}");
-            RuleFor(x => x.Password).NotEmpty().WithMessage("Please, enter {PropertyName}").MinimumLength(7);
             RuleFor(x => x.Gender).NotEmpty().WithMessage("Please, enter {PropertyName}");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Please, enter {PropertyName}").MinimumLength(7);
             RuleFor(x => x.VendorId).NotEmpty().When(x => x.UserTypeId == 0).WithMessage("Please, enter {PropertyName}");
-            RuleFor(x => x.Buyer).NotEmpty().When(x => x.UserTypeId == 1).WithMessage("Please, enter {PropertyName}");
-            //When(x => x.UserTypeId == 0, () =>
-            //{
-            //    RuleFor(b => b.Buyer)
-            //    .Empty()
-            //    .WithMessage("A user cannot be both a Vendor and a Buyer simultaneously.");
-            //});
         }
     }
 }
