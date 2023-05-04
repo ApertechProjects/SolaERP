7d40dd7911163ae8be21659f899f526e995ce6d2
Hulya <hulya.garibli@apertech.net>
2023-05-04 16:42:11 +0400

    bug fixed

--- SolaERP.Infrastructure/Dtos/Buyer/BuyerDto.cs
+++ SolaERP.Infrastructure/Dtos/Buyer/BuyerDto.cs
@@ -5,7 +5,7 @@ namespace SolaERP.Application.Dtos.Buyer
     public class BuyerDto
     {
         public int BusinessUnitId { get; set; }
-        public string BusinesUnitCode { get; set; }
+        public string BusinessUnitCode { get; set; }
         public string BuyerCode { get; set; }
         public string BuyerName { get; set; }
     }
--- SolaERP.Infrastructure/Entities/Buyer/Buyer.cs
+++ SolaERP.Infrastructure/Entities/Buyer/Buyer.cs
@@ -3,7 +3,7 @@
     public class Buyer : BaseEntity
     {
         public int BusinessUnitId { get; set; }
-        public string BusinesUnitCode { get; set; }
+        public string BusinessUnitCode { get; set; }
         public string BuyerCode { get; set; }
         public string BuyerName { get; set; }
     }
