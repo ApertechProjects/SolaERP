949e16547a2433e097b3707f8a4ccea8e9313613
yaqubnasib <yaqubnasibov00@gmail.com>
2023-06-21 17:12:24 +0400

    A

---
+++ SolaERP.Infrastructure/Models/VM_GET_InitialRegistration.cs
@@ -0,0 +1,18 @@
+﻿using SolaERP.Application.Dtos.Attachment;
+using SolaERP.Application.Dtos.SupplierEvaluation;
+using SolaERP.Application.Entities.SupplierEvaluation;
+
+namespace SolaERP.Application.Models
+{
+    public class VM_GET_InitialRegistration
+    {
+        public ContactPersonDto ContactPerson { get; set; }
+        public CompanyInfoDto CompanyInformation { get; set; }
+        public List<BusinessCategory> BusinessCategories { get; set; }
+        public List<PrequalificationCategory> PrequalificationTypes { get; set; }
+        public List<PaymentTerms> PaymentTerms { get; set; }
+        public List<ProductService> Services { get; set; }
+        public List<Country> Countries { get; set; }
+
+    }
+}
