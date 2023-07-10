1f36b8502a7b73f09014b38403da4179e21a05a8
yaqubnasib <yaqubnasibov00@gmail.com>
2023-07-10 12:07:34 +0400

    fix

--- SolaERP.DataAccess/DataAccess/SqlServer/SqlApproveStageDetailRepository.cs
+++ SolaERP.DataAccess/DataAccess/SqlServer/SqlApproveStageDetailRepository.cs
@@ -1,11 +1,11 @@
-﻿using SolaERP.DataAccess.Extensions;
-using SolaERP.Application.Contracts.Repositories;
+﻿using SolaERP.Application.Contracts.Repositories;
 using SolaERP.Application.Entities.ApproveStage;
+using SolaERP.Application.Models;
 using SolaERP.Application.UnitOfWork;
+using SolaERP.DataAccess.Extensions;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
-using SolaERP.Application.Models;
 
 namespace SolaERP.DataAccess.DataAccess.SqlServer
 {
@@ -93,11 +93,11 @@ namespace SolaERP.DataAccess.DataAccess.SqlServer
                     "@NewApproveStageDetailsId = @NewApproveStageDetailsId OUTPUT select @NewApproveStageDetailsId as NewApproveStageDetailsId";
 
                 command.Parameters.AddWithValue(command, "@Id", entity.ApproveStageDetailsId = entity.ApproveStageDetailsId < 0 ? 0 : entity.ApproveStageDetailsId);
-                command.Parameters.AddWithValue(command, "@Id", entity.ApproveStageMainId);
-                command.Parameters.AddWithValue(command, "@ApproveStageDetailsName", entity.ApproveStageDetailsName);
+                command.Parameters.AddWithValue(command, "@Id", entity?.ApproveStageMainId);
+                command.Parameters.AddWithValue(command, "@ApproveStageDetailsName", entity?.ApproveStageDetailsName);
                 command.Parameters.AddWithValue(command, "@Sequence", entity.Sequence);
-                command.Parameters.AddWithValue(command, "@Skip", entity.Skip);
-                command.Parameters.AddWithValue(command, "@SkipDays", entity.SkipDays);
+                command.Parameters.AddWithValue(command, "@Skip", entity?.Skip);
+                command.Parameters.AddWithValue(command, "@SkipDays", entity?.SkipDays);
                 command.Parameters.AddWithValue(command, "@BackToInitiatorOnReject", entity.BackToInitiatorOnReject);
 
                 command.Parameters.Add("@NewApproveStageDetailsId", SqlDbType.Int);
