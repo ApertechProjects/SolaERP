ef36f2529d4be61bfd307d4ca80315da2769ceb3
Hulya <hulya.garibli@apertech.net>
2023-07-20 17:55:31 +0400

    bug fixed

---
+++ SolaERP.Infrastructure/Models/BaseFilterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolaERP.Application.Models
+{
+    public class BaseFilterModel
+    {
+        public int Page { get; set; }
+        public int Limit { get; set; }
+    }
+}
