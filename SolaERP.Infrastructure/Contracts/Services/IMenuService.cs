5bcd599af5b79acec387568bff39d0f57e89fc2d
Hulya <hulya.garibli@apertech.net>
2023-08-02 16:16:34 +0400

    menu changed

--- SolaERP.Infrastructure/Contracts/Services/IMenuService.cs
+++ SolaERP.Infrastructure/Contracts/Services/IMenuService.cs
@@ -9,7 +9,7 @@ namespace SolaERP.Application.Contracts.Services
         Task<ApiResponse<List<ParentMenuDto>>> GetUserMenusWithChildrenAsync(string name);
         Task<ApiResponse<List<MenuWithPrivilagesDto>>> GetUserMenusWithPrivilegesAsync(string name);
         Task<ApiResponse<GroupMenuResponseDto>> GetGroupMenuWithPrivilegeListByGroupIdAsync(string name, int groupId);
-        Task<ApiResponse<List<MenuWithPrivilagesDto>>> GetMenuWithPrivilegeListByGroupIdAsync(int groupId);
+        Task<ApiResponse<List<MenuWithPrivilege>>> GetMenuWithPrivilegeListByGroupIdAsync(int groupId);
         Task<ApiResponse<AdditionalPrivilegeAccessDto>> GetAdditionalPrivilegeAccessAsync(string name);
     }
 }
--- SolaERP.Infrastructure/Dtos/Menu/MenuWithPrivilagesDto.cs
+++ SolaERP.Infrastructure/Dtos/Menu/MenuWithPrivilagesDto.cs
@@ -15,4 +15,26 @@
         public bool ExportAccess { get; set; }
         public string ReactIcon { get; set; }
     }
+
+    public class MenuWithPrivilege
+    {
+        public int Id { get; set; }
+        public string MenuName { get; set; }
+        public List<MenuWithPrivilegeDetail> Details { get; set; }
+    }
+
+    public class MenuWithPrivilegeDetail
+    {
+        public int MenuId { get; set; }
+        public string MenuName { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+        public string MenuCode { get; set; }
+        public bool ReadAccess { get; set; }
+        public bool CreateAccess { get; set; }
+        public bool EditAccess { get; set; }
+        public bool DeleteAccess { get; set; }
+        public bool ExportAccess { get; set; }
+        public string ReactIcon { get; set; }
+    }
 }
