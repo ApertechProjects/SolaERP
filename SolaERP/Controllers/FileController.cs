b6d68ec238c3efc455bcc8018ec1a09f5e1c665a
yaqubnasib <yaqubnasibov00@gmail.com>
2023-08-03 15:26:56 +0400

    File

---
+++ SolaERP/Controllers/FileController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SolaERP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Authorize()
+            => Ok(true);
+    }
+}
