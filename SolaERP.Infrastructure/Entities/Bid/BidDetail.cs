2a3af15c131022784b184f5797337fa0a2c09415
Ibrahim Aliyev <IbrahimA@risk.az>
2023-08-07 15:52:41 +0400

    BidDetail change

---
+++ SolaERP.Infrastructure/Entities/Bid/BidDetail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolaERP.Application.Entities.Bid
+{
+    public class BidDetail
+    {
+        public int BidDetailId { get; set; }
+        public int BidMainId { get; set; }
+        public int RFQDetailId { get; set; }
+        public int LineNO { get; set; }
+        public string ItemCode { get; set; }
+        public bool AlternativeItem { get; set; }
+        public string AlternativeItemCode { get; set; }
+        public bool AlternativeItemName { get; set; }
+        public string PUOM { get; set; }
+        public decimal ConversionRate { get; set; }
+        public string LineDescription { get; set; }
+        public int Condition { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal DiscountType { get; set; }
+        public decimal UnitPrice { get; set;}
+        public decimal SingleUnitPrice { get; set;}
+        public decimal TotalAmount { get; set;}
+        public decimal DiscountedAmount { get; set;}
+        public decimal ConvertedQTY { get; set;}
+        public decimal ConvertedUnitPrice { get; set;}
+    }
+}
