2687d0016c6030fceae2b214ca76dceea12ba8c1
yaqubnasib <yaqubnasibov00@gmail.com>
2023-08-14 15:34:16 +0400

    photo fix

--- SolaERP.Application/Mappers/MapProfile.cs
+++ SolaERP.Application/Mappers/MapProfile.cs
@@ -161,7 +161,7 @@ namespace SolaERP.Persistence.Mappers
             CreateMap<Language, LanguageDto>().ReverseMap();
             CreateMap<Translate, TranslateDto>().ReverseMap();
             CreateMap<VendorInfo, VendorInfoDto>().ReverseMap();
-            CreateMap<UserMain, UserMainDto>().ReverseMap();
+            CreateMap<UserMain, UserMainDto>().ForMember(dest => dest.Photo, opt => opt.MapFrom(src => src.UserPhoto)).ReverseMap();
             CreateMap<UserLoad, UserLoadDto>().ReverseMap();
             CreateMap<ERPUser, ERPUserDto>().ReverseMap();
             CreateMap<AnalysisDimension, AnalysisDimensionDto>().ReverseMap();
@@ -292,7 +292,7 @@ namespace SolaERP.Persistence.Mappers
                  .ForMember(dest => dest.ExtensionType, opt => opt.MapFrom(src => src.ExtensionType))
                  .ForMember(dest => dest.AttachmentTypeId, opt => opt.MapFrom(src => src.AttachmentTypeId))
                  .ForMember(dest => dest.AttachmentSubTypeId, opt => opt.MapFrom(src => src.AttachmentSubTypeId));
-                 //.ForMember(dest => dest.Size, opt => opt.MapFrom(src => src.Size)).ReverseMap();
+            //.ForMember(dest => dest.Size, opt => opt.MapFrom(src => src.Size)).ReverseMap();
 
             CreateMap<AttachmentWithFileDto, AttachmentSaveModel>().ReverseMap();
 
@@ -411,7 +411,7 @@ namespace SolaERP.Persistence.Mappers
             CreateMap<BidAll, BidAllDto>().ReverseMap();
             CreateMap<BidAllFilter, BidAllFilterDto>()
                 .ReverseMap()
-                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => string.Join(",", src.Status.Select(x=> (int)x))))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => string.Join(",", src.Status.Select(x => (int)x))))
                 .ForMember(dest => dest.ApproveStatus, opt => opt.MapFrom(src => string.Join(",", src.ApproveStatus.Select(x => (int)x))))
                 .ForMember(dest => dest.ItemCode, opt => opt.MapFrom(src => string.Join(",", src.ItemCode)))
                 .ForMember(dest => dest.Emergency, opt => opt.MapFrom(src => string.Join(",", src.Emergency.Select(x => (int)x))));
