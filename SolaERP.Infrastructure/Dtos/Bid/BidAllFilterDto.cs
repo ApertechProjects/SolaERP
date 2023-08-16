d3cea7bf5f74f8a9b37a6275f1c43100295252b3
Ibrahim Aliyev <IbrahimA@risk.az>
2023-08-16 12:14:19 +0400

    BidAllFilterDto CodeClean

--- SolaERP.Infrastructure/Dtos/Bid/BidAllFilterDto.cs
+++ SolaERP.Infrastructure/Dtos/Bid/BidAllFilterDto.cs
@@ -1,9 +1,4 @@
 ﻿using SolaERP.Application.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SolaERP.Application.Dtos.Bid
 {
