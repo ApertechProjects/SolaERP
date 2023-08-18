412361e4fe280dc22b5fa1b2075cc37b34ce09e9
Hulya <hulya.garibli@apertech.net>
2023-08-18 17:46:37 +0400

    bug fixed

--- SolaERP.DataAccess/DataAccess/SqlServer/SqlRequestDetailRepository.cs
+++ SolaERP.DataAccess/DataAccess/SqlServer/SqlRequestDetailRepository.cs
@@ -65,7 +65,7 @@ namespace SolaERP.DataAccess.DataAccess.SqlServer
                 command.Parameters.AddWithValue(command, "@RequestedDate", entity.RequestedDate);
                 command.Parameters.AddWithValue(command, "@ItemCode", entity.ItemCode.Trim());
                 command.Parameters.AddWithValue(command, "@Quantity", entity.Quantity);
-                command.Parameters.AddWithValue(command, "@UOM", entity.UOM.Trim());
+                command.Parameters.AddWithValue(command, "@UOM", entity.UOM);
                 command.Parameters.AddWithValue(command, "@Description", entity.Description.Trim());
                 command.Parameters.AddWithValue(command, "@Location", entity.Location.Trim());
                 command.Parameters.AddWithValue(command, "@Buyer", entity.Buyer.Trim());
