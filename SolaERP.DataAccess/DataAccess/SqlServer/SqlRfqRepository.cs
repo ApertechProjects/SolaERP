8a16008f6a447c3e85e5250ee3c8c7b59658e4b6
Ibrahim Aliyev <IbrahimA@risk.az>
2023-08-23 16:54:03 +0400

    RFQ Save fix

--- SolaERP.DataAccess/DataAccess/SqlServer/SqlRfqRepository.cs
+++ SolaERP.DataAccess/DataAccess/SqlServer/SqlRfqRepository.cs
@@ -148,6 +148,7 @@ namespace SolaERP.DataAccess.DataAccess.SqlServer
                 OtherReasons = request.OtherReasons,
                 BusinessCategoryId = request.BusinessCategoryId,
                 UserId = request.UserId,
+                BiddingType = request.BiddingType
             });
         }
 
