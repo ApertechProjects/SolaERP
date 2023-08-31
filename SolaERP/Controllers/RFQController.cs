253d4e2dd92b207d2d711619b0f6d1dff4fef1e9
YagubN <yagubn@risk.az>
2023-08-31 17:39:53 +0400

    Autggorize attribute

--- SolaERP/Controllers/RFQController.cs
+++ SolaERP/Controllers/RFQController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SolaERP.Application.Contracts.Services;
 using SolaERP.Application.Models;
 using SolaERP.Controllers;
@@ -7,6 +8,7 @@ namespace SolaERP.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class RFQController : CustomBaseController
     {
         private readonly IRfqService _service;
