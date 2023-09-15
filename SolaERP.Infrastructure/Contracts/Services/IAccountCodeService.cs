3fed65f620a8388961cdbfefce22c8cd65487b21
chezard <subhanmasimov02@gmail.com>
2023-09-15 13:47:21 +0400

    change to businessUnitI

--- SolaERP.Infrastructure/Contracts/Services/IAccountCodeService.cs
+++ SolaERP.Infrastructure/Contracts/Services/IAccountCodeService.cs
@@ -5,6 +5,6 @@ namespace SolaERP.Application.Contracts.Services
 {
     public interface IAccountCodeService : ICrudService<AccountCodeDto>
     {
-        Task<ApiResponse<List<AccountCodeDto>>> GetAccountCodesByBusinessUnit(string businessUnitCode);
+        Task<ApiResponse<List<AccountCodeDto>>> GetAccountCodesByBusinessUnit(int businesUnitId);
     }
 }
