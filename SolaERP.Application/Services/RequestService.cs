4955efd343c775970be3871a1b02528060a3a359
Hulya <hulya.garibli@apertech.net>
2023-10-02 10:09:22 +0400

    request tabs reverse

--- SolaERP.Application/Services/RequestService.cs
+++ SolaERP.Application/Services/RequestService.cs
@@ -47,7 +47,7 @@ namespace SolaERP.Persistence.Services
         {
             var mainRequest = await _requestMainRepository.GetAllAsync(model);
             var mainRequestDto = _mapper.Map<List<RequestMainDto>>(mainRequest);
-
+            mainRequestDto.Reverse();
             return ApiResponse<List<RequestMainDto>>.Success(mainRequestDto);
         }
 
@@ -131,7 +131,7 @@ namespace SolaERP.Persistence.Services
         {
             var mainDraftEntites = await _requestMainRepository.GetMainRequestDraftsAsync(getMainDraftParameters);
             var mainDraftDto = _mapper.Map<List<RequestMainDraftDto>>(mainDraftEntites);
-
+            mainDraftDto.Reverse();
             return ApiResponse<List<RequestMainDraftDto>>.Success(mainDraftDto);
         }
 
@@ -142,7 +142,7 @@ namespace SolaERP.Persistence.Services
             var mainRequest =
                 await _requestMainRepository.GetApproveAmendmentRequestsAsync(userId, requestParametersDto);
             var mainRequestDto = _mapper.Map<List<RequestAmendmentDto>>(mainRequest);
-
+            mainRequestDto.Reverse();
             return ApiResponse<List<RequestAmendmentDto>>.Success(mainRequestDto);
         }
 
@@ -273,7 +273,7 @@ namespace SolaERP.Persistence.Services
             var mainreq = await _requestMainRepository.GetWaitingForApprovalsAsync(userId, requestWFAGetParametersDto);
 
             var mainRequestDto = _mapper.Map<List<RequestWFADto>>(mainreq);
-
+            mainRequestDto.Reverse();
             if (mainRequestDto != null && mainRequestDto.Count > 0)
                 return ApiResponse<List<RequestWFADto>>.Success(mainRequestDto);
 
@@ -347,7 +347,7 @@ namespace SolaERP.Persistence.Services
             var mainreq = await _requestMainRepository.GetHeldAsync(requestMainGet);
 
             var mainRequestDto = _mapper.Map<List<RequestHeldDto>>(mainreq);
-
+            mainRequestDto.Reverse();
             if (mainRequestDto != null && mainRequestDto.Count > 0)
                 return ApiResponse<List<RequestHeldDto>>.Success(mainRequestDto);
 
