38be47515c936b34f5b7eb67c3aa382edba3de86
Hulya <hulya.garibli@apertech.net>
2023-10-09 16:36:57 +0400

    Bid Comparison date convert to nullable

--- SolaERP.Infrastructure/Dtos/BidComparison/BaseBidComparisonLoadDto.cs
+++ SolaERP.Infrastructure/Dtos/BidComparison/BaseBidComparisonLoadDto.cs
@@ -12,11 +12,11 @@
         public string ComparisonNo { get; set; }
         public string RFQNo { get; set; }
         public string Buyer { get; set; }
-        public DateTime Comparisondeadline { get; set; }
-        public DateTime RFQDeadline { get; set; }
+        public DateTime? Comparisondeadline { get; set; }
+        public DateTime? RFQDeadline { get; set; }
         public string SpecialistComment { get; set; }
         public string CreatedBy { get; set; }
-        public DateTime ComparisonDate { get; set; }
+        public DateTime? ComparisonDate { get; set; }
         public bool HasAttachments { get; set; }
     }
 }
