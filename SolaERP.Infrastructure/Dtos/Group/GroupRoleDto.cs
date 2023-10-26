ed1d166525050f6da10ccc809010db4b8eddbba8
Hulya <hulya.garibli@apertech.net>
2023-10-26 12:53:21 +0400

    group

--- SolaERP.Infrastructure/Dtos/Group/GroupRoleDto.cs
+++ SolaERP.Infrastructure/Dtos/Group/GroupRoleDto.cs
@@ -5,6 +5,7 @@
         public int GroupApproveRoleId { get; set; }
         public int GroupId { get; set; }
         public int ApproveRoleId { get; set; }
+        public string BusinessUnitCode { get; set; }
         public string ApproveRoleName { get; set; }
         public bool IsInGroup { get; set; }
     }
