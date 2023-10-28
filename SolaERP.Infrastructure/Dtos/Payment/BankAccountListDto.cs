5d5eebd9b3cac46b9bf8f929f614c922f0a1d479
chezard <subhanmasimov02@gmail.com>
2023-10-28 15:42:53 +0400

    save

--- SolaERP.Infrastructure/Dtos/Payment/BankAccountListDto.cs
+++ SolaERP.Infrastructure/Dtos/Payment/BankAccountListDto.cs
@@ -11,5 +11,6 @@ namespace SolaERP.Application.Dtos.Payment
         public string BankAccount { get; set; }
         public string CurrencyCode { get; set; }
         public decimal Amount { get; set; }
+        public string AccountName { get; set; }
     }
 }
