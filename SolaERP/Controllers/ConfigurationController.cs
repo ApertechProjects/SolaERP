e7c5603b96ee6ff86a33d6473693e413c80e16af
Hulya <hulya.garibli@apertech.net>
2023-11-15 18:14:24 +0400

    bug fixed

---
+++ SolaERP/Controllers/ConfigurationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SolaERP.Application.Helper;
+using System.Text.Json;
+
+namespace SolaERP.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ConfigurationController : ControllerBase
+    {
+        //private readonly IConfiguration _configuration;
+
+        public ConfigurationController()
+        {
+            //_configuration = configuration;
+        }
+
+        [HttpGet]
+        public string GetConfiguration()
+        {
+            string appsettingsFileName = AppSettingsHelper.GetAppSettingsFileName();
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+            .AddJsonFile(appsettingsFileName, optional: true, reloadOnChange: true);
+
+            IConfiguration _configuration = builder.Build();
+
+            return _configuration["Mail:Url"];
+        }
+    }
+}
