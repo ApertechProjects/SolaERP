70502a5588fd2fe51426125857e23519b08013b6
Hulya <hulya.garibli@apertech.net>
2023-11-30 15:24:02 +0400

    bug fixed

--- SolaERP.Infrastructure/Entities/Invoice/RegisterListByOrder.cs
+++ SolaERP.Infrastructure/Entities/Invoice/RegisterListByOrder.cs
@@ -5,5 +5,9 @@
         public int InvoiceRegisterId { get; set; }
         public string InvoiceNo { get; set; }
         public decimal InvoiceAmount { get; set; }
+        public DateTime InvoiceReceivedDate { get; set; }
+        public string InvoiceComment { get; set; }
+        public decimal OrderTotal { get; set; }
+        public string WithHoldingTaxCode { get; set; }
     }
 }
