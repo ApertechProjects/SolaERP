6eb5007c2eecd70a11bfd657c87ec8ba9525d875
Hulya <hulya.garibli@apertech.net>
2023-12-11 10:46:24 +0400

    Account Code and account name added

--- SolaERP.Infrastructure/Entities/Item Code/ItemCode.cs
+++ SolaERP.Infrastructure/Entities/Item Code/ItemCode.cs
@@ -10,5 +10,7 @@ namespace SolaERP.Application.Entities.Item_Code
         public string LongDescription { get; set; }
         public string UnitOfPurch { get; set; }
         public string ItemDescriptionAze { get; set; }
+        public string AccountCode { get; set; }
+        public string AccountName { get; set; }
     }
 }
