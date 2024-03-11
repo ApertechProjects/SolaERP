96bbfbcf513cccff29f3059294367804d1d341f1
chezard <subhanmasimov02@gmail.com>
2024-03-11 17:50:17 +0400

    fix

--- SolaERP.Infrastructure/Models/PaymentRequestGetModel.cs
+++ SolaERP.Infrastructure/Models/PaymentRequestGetModel.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SolaERP.Application.Models
+﻿namespace SolaERP.Application.Models
 {
     public class PaymentRequestGetModel
     {
         public string VendorCode { get; set; }
         public string CurrencyCode { get; set; }
+        public int BusinessUnitId { get; set; }
     }
 }
