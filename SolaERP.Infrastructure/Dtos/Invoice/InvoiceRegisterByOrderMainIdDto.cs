d3d0165c6a662551018cc839033a9c6330c4122e
ApertechProjects <info@apertech.net>
2024-04-02 11:32:55 +0400

    invoice Received date added

--- SolaERP.Infrastructure/Dtos/Invoice/InvoiceRegisterByOrderMainIdDto.cs
+++ SolaERP.Infrastructure/Dtos/Invoice/InvoiceRegisterByOrderMainIdDto.cs
@@ -16,5 +16,7 @@ namespace SolaERP.Application.Dtos.Invoice
         public int WithHoldingTaxId { get; set; }
         public string WithHoldingTaxCode { get; set; }
         public decimal OrderTotal { get; set; }
+        public DateTime InvoiceReceivedDate { get; set; }
+
     }
 }
