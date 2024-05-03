c8c6e28203752ef4ad7125845d81325e789ce985
ApertechProjects <info@apertech.net>
2024-05-03 15:50:05 +0400

    line no added

--- SolaERP.Infrastructure/Dtos/Request/RequestApprovalInfoDto.cs
+++ SolaERP.Infrastructure/Dtos/Request/RequestApprovalInfoDto.cs
@@ -9,5 +9,6 @@
         public string Comment { get; set; }
         public int ApproveStatus { get; set; }
         public string ApprovalStatusName { get; set; }
+        public string LineNo { get; set; }
     }
 }
