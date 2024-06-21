c3eac8f0a5e4145fdc3d764473afb1a4f06233f1
ApertechProjects <info@apertech.net>
2024-06-21 10:44:26 +0400

    bug fixed

--- SolaERP.Job/Cbar/CbarBackgroundJobSetup.cs
+++ SolaERP.Job/Cbar/CbarBackgroundJobSetup.cs
@@ -15,43 +15,50 @@ namespace SolaERP.Job.Cbar
         {
             try
             {
+                //var jobKey = JobKey.Create(nameof(CbarBackgroundJob));
+                //options.AddJob<CbarBackgroundJob>(jobBuilder => jobBuilder.WithIdentity(jobKey));
+
+                //// Add trigger for 10:05 AM
+                //options.AddTrigger(trigger => trigger
+                //    .ForJob(jobKey)
+                //    .WithIdentity("trigger1", "group1")
+                //    .WithCronSchedule("0 05 10 * * ?")
+                //    .Build());
+
+                //// Add trigger for 10:35 AM
+                //options.AddTrigger(trigger => trigger
+                //    .ForJob(jobKey)
+                //    .WithIdentity("trigger2", "group1")
+                //    .WithCronSchedule("0 35 10 * * ?")
+                //    .Build());
+
+                //// Add trigger for 11:05 AM
+                //options.AddTrigger(trigger => trigger
+                //    .ForJob(jobKey)
+                //    .WithIdentity("trigger3", "group1")
+                //    .WithCronSchedule("0 5 11 * * ?")
+                //    .Build());
+
+                //// Add trigger for 11:35 AM
+                //options.AddTrigger(trigger => trigger
+                //    .ForJob(jobKey)
+                //    .WithIdentity("trigger4", "group1")
+                //    .WithCronSchedule("0 35 11 * * ?")
+                //    .Build());
+
+                //// Add trigger for 12:05 PM
+                //options.AddTrigger(trigger => trigger
+                //    .ForJob(jobKey)
+                //    .WithIdentity("trigger5", "group1")
+                //    .WithCronSchedule("0 5 12 * * ?")
+                //    .Build());
+
                 var jobKey = JobKey.Create(nameof(CbarBackgroundJob));
-                options.AddJob<CbarBackgroundJob>(jobBuilder => jobBuilder.WithIdentity(jobKey));
-
-                // Add trigger for 10:05 AM
-                options.AddTrigger(trigger => trigger
-                    .ForJob(jobKey)
-                    .WithIdentity("trigger1", "group1")
-                    .WithCronSchedule("0 05 10 * * ?")
-                    .Build());
-
-                // Add trigger for 10:35 AM
-                options.AddTrigger(trigger => trigger
-                    .ForJob(jobKey)
-                    .WithIdentity("trigger2", "group1")
-                    .WithCronSchedule("0 35 10 * * ?")
-                    .Build());
-
-                // Add trigger for 11:05 AM
-                options.AddTrigger(trigger => trigger
-                    .ForJob(jobKey)
-                    .WithIdentity("trigger3", "group1")
-                    .WithCronSchedule("0 5 11 * * ?")
-                    .Build());
-
-                // Add trigger for 11:35 AM
-                options.AddTrigger(trigger => trigger
-                    .ForJob(jobKey)
-                    .WithIdentity("trigger4", "group1")
-                    .WithCronSchedule("0 35 11 * * ?")
-                    .Build());
-
-                // Add trigger for 12:05 PM
-                options.AddTrigger(trigger => trigger
-                    .ForJob(jobKey)
-                    .WithIdentity("trigger5", "group1")
-                    .WithCronSchedule("0 5 12 * * ?")
-                    .Build());
+                options.AddJob<CbarBackgroundJob>(jobBuilder => jobBuilder.WithIdentity(jobKey))
+                       .AddTrigger(trigger => trigger
+                       .ForJob(jobKey)
+                       .WithCronSchedule("0 0/10 9-18 * * ?")
+                       .Build());
 
                 Console.WriteLine("Job Run" + DateTime.Now);
             }
