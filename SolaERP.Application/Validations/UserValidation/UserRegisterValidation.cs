4375c229082fcbe6599c277b59f60016cc0f7498
ApertechProjects <info@apertech.net>
2024-08-14 15:35:41 +0400

    Phone number validation addedd

--- SolaERP.Application/Validations/UserValidation/UserRegisterValidation.cs
+++ SolaERP.Application/Validations/UserValidation/UserRegisterValidation.cs
@@ -9,11 +9,12 @@ namespace SolaERP.Application.Validations.UserValidation
         {
             RuleFor(x => x.FullName).NotEmpty().WithMessage("Please, enter {PropertyName}");
             RuleFor(x => x.UserName).NotEmpty().WithMessage("Please, enter {PropertyName}");
+            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Please, enter {PropertyName}");
             RuleFor(x => x.Email).NotEmpty().WithMessage("Please, enter {PropertyName}").EmailAddress().WithMessage("Please, enter valid {PropertyName}")
                 .Matches(@"^[^@\s]+@[^@\s]+\.[^@\s]+$").WithMessage("Please, enter valid {PropertyName}");
             RuleFor(x => x.Password).NotEmpty().WithMessage("Please, enter {PropertyName}").MinimumLength(7).WithMessage("Password minimum length must be 7 symbol"); ;
             RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Please, enter {PropertyName}").MinimumLength(7).WithMessage("Password minimum length must be 7 symbol"); ;
-            RuleFor(x => x.Gender).GreaterThanOrEqualTo(1).WithMessage("Please, enter {PropertyName}").NotEmpty().WithMessage("Please, enter {PropertyName}").NotNull().ExclusiveBetween(0,3);
+            RuleFor(x => x.Gender).GreaterThanOrEqualTo(1).WithMessage("Please, enter {PropertyName}").NotEmpty().WithMessage("Please, enter {PropertyName}").NotNull().ExclusiveBetween(0, 3);
         }
     }
 }
