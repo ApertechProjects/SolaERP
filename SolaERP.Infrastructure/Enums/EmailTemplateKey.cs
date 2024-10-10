47f40ae1190c3abae4a8400865a275ecb987c095
ApertechProjects <info@apertech.net>
2024-10-10 13:25:45 +0400

    auto approve email added

--- SolaERP.Infrastructure/Enums/EmailTemplateKey.cs
+++ SolaERP.Infrastructure/Enums/EmailTemplateKey.cs
@@ -1,33 +1,34 @@
 ﻿namespace SolaERP.Application.Enums
 {
-    public enum EmailTemplateKey
-    {
-        VER,  //verification Email
-        RGA, //Registration Pending For Approval for single user
-        RP,  //Registration Pending for group
-        REQP, //Request is pending
-        REQA, //Request is approved
-        REQR, //Request is rejected
-        REQH, //Request is held
-        UP, //User is pending
-        UA, //User Approved
-        UR, //User Reject
-        UH //User Held
-    }
+	public enum EmailTemplateKey
+	{
+		VER,  //verification Email
+		RGA, //Registration Pending For Approval for single user
+		RP,  //Registration Pending for group
+		REQP, //Request is pending
+		REQA, //Request is approved
+		REQR, //Request is rejected
+		REQH, //Request is held
+		UP, //User is pending
+		UA, //User Approved
+		UR, //User Reject
+		UH, //User Held
+		AAI //Auto Approve Information
+	}
 
 
-    public enum EmailTemplate
-    {
-        EmailConfirmPage,
-        EmailVerification,  //verification Email
-        RegistrationPendingForApproval, //Registration Pending For Approval for single user
-        RequestPending, //Request is pending
-        RequestApproved, //Request is approved
-        RequestRejected, //Request is rejected
-        RequestHeld, //Request is held
-        UserPending, //User is pending
-        UserApproved, //User Approved
-        UserRejected, //User Reject
-        UserHeld //User Held
-    }
+	public enum EmailTemplate
+	{
+		EmailConfirmPage,
+		EmailVerification,  //verification Email
+		RegistrationPendingForApproval, //Registration Pending For Approval for single user
+		RequestPending, //Request is pending
+		RequestApproved, //Request is approved
+		RequestRejected, //Request is rejected
+		RequestHeld, //Request is held
+		UserPending, //User is pending
+		UserApproved, //User Approved
+		UserRejected, //User Reject
+		UserHeld //User Held
+	}
 }
