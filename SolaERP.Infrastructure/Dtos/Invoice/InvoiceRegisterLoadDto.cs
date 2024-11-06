3eee702a800206f71b0e907b5215e878efdcd4b5
ApertechProjects <info@apertech.net>
2024-11-06 15:42:59 +0400

    bug fixed

--- SolaERP.Infrastructure/Dtos/Invoice/InvoiceRegisterLoadDto.cs
+++ SolaERP.Infrastructure/Dtos/Invoice/InvoiceRegisterLoadDto.cs
@@ -37,6 +37,6 @@ namespace SolaERP.Application.Dtos.Invoice
 		public int? InvoicePeriod { get; set; }
 		public string? VendorAccount { get; set; }
 		public bool UseOrderForInvoice { get; set; }
-		public List<InvoiceRegisterGetDetailsDto> InvoiceRegisterDetails { get; set; }
+		public List<InvoiceRegisterGetDetailsDto> Details { get; set; }
 	}
 }
