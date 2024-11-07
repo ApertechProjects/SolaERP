79ca320975dac5a952b3c162cb3adbfaa57323b7
ApertechProjects <info@apertech.net>
2024-11-07 11:33:52 +0400

    bug fuxed

--- SolaERP.Infrastructure/Dtos/Invoice/InvoiceRegisterGetDetailsDto.cs
+++ SolaERP.Infrastructure/Dtos/Invoice/InvoiceRegisterGetDetailsDto.cs
@@ -23,5 +23,15 @@ namespace SolaERP.Application.Dtos.Invoice
 		public decimal ReportingTaxAmount { get; set; }
 		public decimal BaseGrossAmount { get; set; }
 		public decimal ReportingGrossAmount { get; set; }
+		public int AnalysisCode1Id { get; set; }
+		public int AnalysisCode2Id { get; set; }
+		public int AnalysisCode3Id { get; set; }
+		public int AnalysisCode4Id { get; set; }
+		public int AnalysisCode5Id { get; set; }
+		public int AnalysisCode6Id { get; set; }
+		public int AnalysisCode7Id { get; set; }
+		public int AnalysisCode8Id { get; set; }
+		public int AnalysisCode9Id { get; set; }
+		public int AnalysisCode10Id { get; set; }
 	}
 }
