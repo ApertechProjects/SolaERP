3f6f8ba4e4844e879ea477d9f0c7368dec67a901
ApertechProjects <info@apertech.net>
2024-11-19 16:57:00 +0400

    business unit

--- SolaERP.Infrastructure/Dtos/BusinessUnit/BaseBusinessUnitDto.cs
+++ SolaERP.Infrastructure/Dtos/BusinessUnit/BaseBusinessUnitDto.cs
@@ -6,6 +6,6 @@
         public string BusinessUnitCode { get; set; }
         public string BusinessUnitName { get; set; }
         public string BankChargeAccount { get; set; }
-
+		public bool UseOrderForInvoice { get; set; }
     }
 }
