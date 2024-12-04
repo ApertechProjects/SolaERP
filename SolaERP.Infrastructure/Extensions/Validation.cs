ec02c841889e41cbcf10256d77df32e704181117
ApertechProjects <info@apertech.net>
2024-12-04 16:33:14 +0400

    validation added

---
+++ SolaERP.Infrastructure/Extensions/Validation.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolaERP.Application.Extensions
+{
+	public static class Validation
+	{
+		public static bool PhotoIsValid(this IFormFile formFile)
+		{
+			if (formFile != null)
+			{
+				string fileExtensionPhoto = Path.GetExtension(formFile.FileName);
+				if (fileExtensionPhoto != ".png"
+					&& fileExtensionPhoto != ".jpeg"
+					&& fileExtensionPhoto != ".jpg")
+					return false;
+			}
+			return true;
+		}
+	}
+}
