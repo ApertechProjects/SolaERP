4f94240a1fd9aa24c55e5759cc209ccdbcb16028
Hulya <Hulya@DESKTOP-S24GON5>
2024-12-05 00:29:04 +0400

    some changes added

--- SolaERP.Infrastructure/Entities/Invoice/MatchingMain.cs
+++ SolaERP.Infrastructure/Entities/Invoice/MatchingMain.cs
@@ -7,5 +7,15 @@ public class MatchingMain : BaseEntity
     public string VendorCode { get; set; }
     public string VendorName { get; set; }
     public string Currency { get; set; }
+    public string OrderComment { get; set; }
+    public string WithHoldingTaxCode { get; set; }
+    public int BusinessUnitId { get; set; }
+    public int InvoiceRegisterId { get; set; }
+    public decimal WHT { get; set; }
     public string Comment { get; set; }
+    public DateTime TransactionDate { get; set; }
+    public decimal TaxAmount { get; set; }
+    public decimal GrossAmount { get; set; }
+    public decimal OrderAmount { get; set; }
+    public decimal SupplierWHTRate { get; set; }
 }
