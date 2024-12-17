5572e0e37898f966e0b02b2a29e23b8f2bd34042
ApertechProjects <info@apertech.net>
2024-12-17 11:44:34 +0400

    password strong validate

--- SolaERP.Application/Validations/UserValidation/ResetPasswordRequestValidation.cs
+++ SolaERP.Application/Validations/UserValidation/ResetPasswordRequestValidation.cs
@@ -7,8 +7,19 @@ namespace SolaERP.Application.Validations.UserValidation
     {
         public ResetPasswordRequestValidation()
         {
-            RuleFor(x => x.Password).NotEmpty().WithMessage("Please enter {PropertyName}").MinimumLength(7).WithMessage("Password minimum length must be 7 symbol");
-            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Please enter {PropertyName}").MinimumLength(7).WithMessage("Password minimum length must be 7 symbol");
-        }
+			RuleFor(x => x.Password)
+		   .NotEmpty()
+		   .WithMessage("Password is required.")
+		   .MinimumLength(7)
+		   .WithMessage("Password must be at least 8 characters long.")
+		   .Matches("[A-Z]")
+		   .WithMessage("Password must contain at least one uppercase letter.")
+		   .Matches("[a-z]")
+		   .WithMessage("Password must contain at least one lowercase letter.")
+		   .Matches("[0-9]")
+		   .WithMessage("Password must contain at least one number.")
+		   .Matches("[^a-zA-Z0-9]")
+		   .WithMessage("Password must contain at least one special character.");
+		}
     }
 }
