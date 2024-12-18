8e4f6ba75eecf5a7233befd89b055887b1b09345
Hulya <Hulya@DESKTOP-S24GON5>
2024-12-18 23:59:18 +0400

    rfq mail added

--- SolaERP.Job/Send.cs
+++ SolaERP.Job/Send.cs
@@ -17,87 +17,91 @@ using System.Threading.Tasks;
 
 namespace SolaERP.Job
 {
-	public class Send : ISend
-	{
-		private readonly ILogger<EmailIsSentForAssignedBuyer> _logger;
-		private readonly IBackgroundMailService _backgroundMailService;
-		private readonly IMailService _mailService;
-		private readonly IEmailNotificationService _emailNotificationService;
-		private readonly IUnitOfWork _unitOfWork;
-		private readonly IMapper _mapper;
-		public Send(ILogger<EmailIsSentForAssignedBuyer> logger,
-								  IUnitOfWork unitOfWork,
-								  IBackgroundMailService backgroundMailService,
-								  IMailService mailService, IEmailNotificationService emailNotificationService,
-		IMapper mapper)
-		{
-			_logger = logger;
-			Console.WriteLine("constructor started");
-			Debug.WriteLine("constructor started");
-			_unitOfWork = unitOfWork;
-			_backgroundMailService = backgroundMailService;
-			_mailService = mailService;
-			_mapper = mapper;
-			_emailNotificationService = emailNotificationService;
-			Console.WriteLine("constructor finsihed");
-			Debug.WriteLine("constructor finsihed");
-		}
-		public async Task SendRequestMails(StatusType statusType)
-		{
-			try
-			{
-				Console.WriteLine("Execute started");
-				Helper helper = new Helper(_unitOfWork);
-				var requestUsers = helper.GetUsersIsSent(Procedure.Request);
-				if (requestUsers != null && requestUsers.Count > 0)
-				{
-					foreach (var user in requestUsers)
-					{
-						Debug.WriteLine("sended to " + user.UserName);
-						Console.WriteLine("sended to " + user.UserName);
-						var rowInfoDrafts = helper.GetRowInfosForIsSent(Procedure.Request, user.UserId, statusType);
+    public class Send : ISend
+    {
+        private readonly ILogger<EmailIsSentForAssignedBuyer> _logger;
+        private readonly IBackgroundMailService _backgroundMailService;
+        private readonly IMailService _mailService;
+        private readonly IEmailNotificationService _emailNotificationService;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public Send(ILogger<EmailIsSentForAssignedBuyer> logger,
+                                  IUnitOfWork unitOfWork,
+                                  IBackgroundMailService backgroundMailService,
+                                  IMailService mailService, IEmailNotificationService emailNotificationService,
+        IMapper mapper)
+        {
+            _logger = logger;
+            Console.WriteLine("constructor started");
+            Debug.WriteLine("constructor started");
+            _unitOfWork = unitOfWork;
+            _backgroundMailService = backgroundMailService;
+            _mailService = mailService;
+            _mapper = mapper;
+            _emailNotificationService = emailNotificationService;
+            Console.WriteLine("constructor finsihed");
+            Debug.WriteLine("constructor finsihed");
+        }
+        public async Task SendRequestMails(StatusType statusType)
+        {
+            try
+            {
+                Console.WriteLine("Execute started");
+                Helper helper = new Helper(_unitOfWork);
+                var requestUsers = helper.GetUsersIsSent(Procedure.Request);
+                if (requestUsers != null && requestUsers.Count > 0)
+                {
+                    foreach (var user in requestUsers)
+                    {
+                        Debug.WriteLine("sended to " + user.UserName);
+                        Console.WriteLine("sended to " + user.UserName);
+                        var rowInfoDrafts = helper.GetRowInfosForIsSent(Procedure.Request, user.UserId, statusType);
 
-						var rowInfos = _mapper.Map<HashSet<RowInfo>>(rowInfoDrafts);
-						if (rowInfos.Count > 0)
-						{
-							await _backgroundMailService.SendMailAsync(rowInfos, new Person { email = user.Email, lang = user.Language, userName = user.UserName });
-							Console.WriteLine("Log: " + "Mail");
-							int[] ids = rowInfoDrafts.Select(x => x.notificationSenderId).ToArray();
-							helper.UpdateIsSent1(ids);
-							await _unitOfWork.SaveChangesAsync();
-						}
-					}
-				}
-			}
-			catch (Exception ex)
-			{
-				_logger.LogError(ex, "An error occurred while executing the email background job 1.");
-				Console.WriteLine("Exception: " + ex.Message);
-			}
-			await Task.CompletedTask;
-		}
+                        var rowInfos = _mapper.Map<HashSet<RowInfo>>(rowInfoDrafts);
+                        if (rowInfos.Count > 0)
+                        {
+                            await _backgroundMailService.SendMailAsync(rowInfos, new Person { email = user.Email, lang = user.Language, userName = user.UserName });
+                            Console.WriteLine("Log: " + "Mail");
+                            int[] ids = rowInfoDrafts.Select(x => x.notificationSenderId).ToArray();
+                            helper.UpdateIsSent1(ids);
+                            await _unitOfWork.SaveChangesAsync();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while executing the email background job 1.");
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            await Task.CompletedTask;
+        }
 
-		public async Task SendRFQCloseMails()
-		{
-			RFQMethods methods = new RFQMethods(_unitOfWork);
-			var users = await methods.GetRFQVendorUsers(1411);
+        public async Task SendRFQCloseMails()
+        {
+            RFQMethods methods = new RFQMethods(_unitOfWork);
+            var closedRFQs = methods.GetClosedRFQs();
 
-			foreach (var user in users)
-			{
-				var companyName = await _emailNotificationService.GetCompanyName(user.UserName);
-				VM_RFQClose rfqClose = new VM_RFQClose(user.Language, user.FullName)
-				{
-					CompanyName = companyName,
-					Language = (Language)Enum.Parse(typeof(Language), user.Language.ToString())
-				};
-
-
-				await _mailService.SendUsingTemplate(rfqClose.Subject, rfqClose,
-				rfqClose.TemplateName(), null,
-				new List<string> { user.UserName });
+            foreach (var rfqId in closedRFQs)
+            {
+                var users = await methods.GetRFQVendorUsers(rfqId);
+                foreach (var user in users)
+                {
+                    var companyName = await _emailNotificationService.GetCompanyName(user.UserName);
+                    VM_RFQClose rfqClose = new VM_RFQClose(user.Language, user.FullName)
+                    {
+                        CompanyName = companyName,
+                        Language = (Language)Enum.Parse(typeof(Language), user.Language.ToString())
+                    };
 
 
-			}
-		}
-	}
+                    await _mailService.SendUsingTemplate(rfqClose.Subject, rfqClose,
+                    rfqClose.TemplateName(), null,
+                    new List<string> { user.UserName });
+
+
+                }
+            }
+        }
+    }
 }
