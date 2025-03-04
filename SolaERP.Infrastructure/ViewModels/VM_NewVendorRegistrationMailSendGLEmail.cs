0cdf82619d0d8c79414bf0a514649177a263d270
anar_ceferov <anarceferov1996@gmail.com>
2025-03-04 17:15:13 +0400

    SendNewVendorRegistrationMail bugfix

--- SolaERP.Infrastructure/ViewModels/VM_NewVendorRegistrationMailSendGLEmail.cs
+++ SolaERP.Infrastructure/ViewModels/VM_NewVendorRegistrationMailSendGLEmail.cs
@@ -41,32 +41,28 @@ namespace SolaERP.Application.ViewModels
 		public HtmlString GetBodyOfMailAz()
 		{
 			return new HtmlString(
-				$"<div style='font-family: Arial, sans-serif; font-size: 14px; line-height: 1.5;'>" +
-				"<table width='100%' style='border-spacing: 0; padding: 0;'>" +
+				"<table width='100%' style='font-family: Arial, sans-serif; font-size: 14px; line-height: 1.5; border-spacing: 0; padding: 0;'>" +
 				"<tr><td><p>Hörmətli Administrator,</p></td></tr>" +
 				"<tr><td><p>Aşağıda qeyd olunan yeni vendor qeydiyyatdan keçmişdir. Xahiş edirik, vendor məlumatlarını nəzərdən keçirdib, təsdiq mərhələsinə göndərin.</p></td></tr>" +
 				$"<tr><td><p><b>Vendor Adı: {_vendorName}</b></p></td></tr>" +
-				"<tr><td><p><b><a href='http://sola.glcorp.ltd/vendors' style='text-decoration: none; color: #0073e6;'>Təsdiq üçün daxil olun</a></b></p></td></tr>" +
+				"<tr><td><p><b><a href='http://sola.glcorp.ltd/vendors' style='text-decoration: none;'>Təsdiq üçün daxil olun</a></b></p></td></tr>" +
 				"<tr><td><p>Əlavə suallarınız olarsa, bizimlə əlaqə saxlamaqdan çəkinməyin.</p></td></tr>" +
 				"<tr><td><p>Hörmətlə,<br>GL Group</p></td></tr>" +
-				"</table>" +
-				"</div>"
+				"</table>"
 			);
 		}
     
 		public HtmlString GetBodyOfMailEn()
 		{
 			return new HtmlString(
-				"<div style='font-family: Arial, sans-serif; font-size: 14px; line-height: 1.5;'>" +
-				"<table width='100%' style='border-spacing: 0; padding: 0;'>" +
+				"<table width='100%' style='font-family: Arial, sans-serif; font-size: 14px; line-height: 1.5; border-spacing: 0; padding: 0;'>" +
 				"<tr><td><p>Dear Administrator,</p></td></tr>" +
 				"<tr><td><p>The new vendor below has registered. Please review the vendor information and proceed with the approval process.</p></td></tr>" +
 				$"<tr><td><p><b>Vendor Name: {_vendorName}</b></p></td></tr>" +
-				"<tr><td><p><b><a href='http://sola.glcorp.ltd/vendors' style='text-decoration: none; color: #0073e6;'>Login for approval</a></b></p></td></tr>" +
+				"<tr><td><p><b><a href='http://sola.glcorp.ltd/vendors' style='text-decoration: none;'>Login for approval</a></b></p></td></tr>" +
 				"<tr><td><p>If you have any further questions, please do not hesitate to contact us.</p></td></tr>" +
 				"<tr><td><p>Best regards,<br>GL Group</p></td></tr>" +
-				"</table>" +
-				"</div>"
+				"</table>"
 			);
 		}
 	}
