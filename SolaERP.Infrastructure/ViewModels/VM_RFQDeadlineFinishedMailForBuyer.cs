1b7b4a38fce668394485d3589a87ad4889193803
anar_ceferov <anarceferov1996@gmail.com>
2025-04-07 16:48:12 +0400

    RFQDeadlineFinishedMailForBuyer

--- SolaERP.Infrastructure/ViewModels/VM_RFQDeadlineFinishedMailForBuyer.cs
+++ SolaERP.Infrastructure/ViewModels/VM_RFQDeadlineFinishedMailForBuyer.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Html;
 using Microsoft.Extensions.Configuration;
+using SolaERP.Application.Helper;
 using SolaERP.Infrastructure.ViewModels;
 
 namespace SolaERP.Application.ViewModels
@@ -15,6 +16,10 @@ namespace SolaERP.Application.ViewModels
         public VM_RFQDeadlineFinishedMailForBuyer(string lang, string buyerName, string rfqNo, DateTime rfqDeadline,
             int rfqId)
         {
+            string appsettingsFileName = AppSettingsHelper.GetAppSettingsFileName();
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile(appsettingsFileName, optional: true, reloadOnChange: false);
+
+            _configuration = builder.Build();
             _buyerName = buyerName;
             _rfqNo = rfqNo;
             _rfqDeadline = rfqDeadline;
@@ -47,7 +52,7 @@ namespace SolaERP.Application.ViewModels
                 "<table width='100%' style='font-family: Arial, sans-serif; font-size: 14px; line-height: 1.5; border-spacing: 0; padding: 0;'>" +
                 $"<tr><td><p>Hörmətli {_buyerName},</p></td></tr>" +
                 $"<tr><td><p>{_rfqNo} nömrəli Qiymət Təklifi Sorğusunun son müraciət tarixi {_rfqDeadline} tarixində bitmişdir.</p></td></tr>" +
-                $"<tr><td><p>Sorğuya baxmaq üçün <b><a href={_configuration["Mail:ServerUrlUI"] + "/" + _rfqId} style='text-decoration: none;'> linkə </a></b> keçid edə bilərsiniz.</p></td></tr>" +
+                $"<tr><td><p>Sorğuya baxmaq üçün <b><a href='{_configuration["Mail:ServerUrlUI"] + "/" + _rfqId}' style='text-decoration: none;'> linkə </a></b> keçid edə bilərsiniz.</p></td></tr>"+
                 "<tr><td><p>Hörmətlə,<br>GL Group</p></td></tr>" +
                 "</table>"
             );
@@ -59,7 +64,7 @@ namespace SolaERP.Application.ViewModels
                 "<table width='100%' style='font-family: Arial, sans-serif; font-size: 14px; line-height: 1.5; border-spacing: 0; padding: 0;'>" +
                 $"<tr><td><p>Dear {_buyerName},</p></td></tr>" +
                 $"<tr><td><p>The deadline for the submission of the {_rfqNo} number was {_rfqDeadline}.</p></td></tr>" +
-                $"<tr><td><p>You can review the RFQ using the following <b><a href={_configuration["Mail:ServerUrlUI"] + "/" + _rfqId} style='text-decoration: none;'> link </a></b>.</p></td></tr>" +
+                $"<tr><td><p>You can review the RFQ using the following <b><a href='{_configuration["Mail:ServerUrlUI"] + "/" + _rfqId}' style='text-decoration: none;'> link </a></b>.</p></td></tr>" +
                 "<tr><td><p>Best regards,<br>GL Group</p></td></tr>" +
                 "</table>"
             );
