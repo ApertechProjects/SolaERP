33cc3847b7b44c918bbac15b29e3bdd48b250dc6
chezard <subhanmasimov02@gmail.com>
2025-04-26 13:08:02 +0400

    add updates

--- SolaERP.Infrastructure/Dtos/Payment/PaymentOrderMainDto.cs
+++ SolaERP.Infrastructure/Dtos/Payment/PaymentOrderMainDto.cs
@@ -50,5 +50,16 @@
         public string WellNo { get; set; }
         public string LinkAccount { get; set; }
         public string Department { get; set; }
+        public string AnalysisCode1 { get; set; }
+        public string AnalysisCode2 { get; set; }
+        public string AnalysisCode3 { get; set; }
+        public string AnalysisCode4 { get; set; }
+        public string AnalysisCode5 { get; set; }
+        public string AnalysisCode6 { get; set; }
+        public string AnalysisCode7 { get; set; }
+        public string AnalysisCode8 { get; set; }
+        public string AnalysisCode9 { get; set; }
+        public string AnalysisCode10 { get; set; }
+
     }
 }
--- SolaERP.Infrastructure/Models/PaymentOrderPostModel.cs
+++ SolaERP.Infrastructure/Models/PaymentOrderPostModel.cs
@@ -65,16 +65,16 @@ namespace SolaERP.Application.Models
         public string LinkAccount { get; set; }
         public string VendorCode { get; set; }
         public string Department { get; set; }
-        public string? AnalysisCode1 { get; set; }
-        public string? AnalysisCode2 { get; set; }
-        public string? AnalysisCode3 { get; set; }
-        public string? AnalysisCode4 { get; set; }
-        public string? AnalysisCode5 { get; set; }
-        public string? AnalysisCode6 { get; set; }
-        public string? AnalysisCode7 { get; set; }
-        public string? AnalysisCode8 { get; set; }
-        public string? AnalysisCode9 { get; set; }
-        public string? AnalysisCode10 { get; set; }
+        public string AnalysisCode1 { get; set; }
+        public string AnalysisCode2 { get; set; }
+        public string AnalysisCode3 { get; set; }
+        public string AnalysisCode4 { get; set; }
+        public string AnalysisCode5 { get; set; }
+        public string AnalysisCode6 { get; set; }
+        public string AnalysisCode7 { get; set; }
+        public string AnalysisCode8 { get; set; }
+        public string AnalysisCode9 { get; set; }
+        public string AnalysisCode10 { get; set; }
     }
 
     public class PaymentTransaction
