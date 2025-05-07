286369c63505a70e421d30d0abcc92631badc1a2
chezard <subhanmasimov02@gmail.com>
2025-05-07 14:35:15 +0400

    add

--- SolaERP.Infrastructure/Dtos/Payment/PaymentOrderMainDto.cs
+++ SolaERP.Infrastructure/Dtos/Payment/PaymentOrderMainDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace SolaERP.Application.Dtos.Payment
 {
-    public class PaymentOrderMainDto 
+    public class PaymentOrderMainDto
     {
         public int BusinessUnitId { get; set; }
         public string VendorCode { get; set; }
@@ -60,6 +60,36 @@
         public string AnalysisCode8 { get; set; }
         public string AnalysisCode9 { get; set; }
         public string AnalysisCode10 { get; set; }
-
+        public string GNRL_DESCR_01 { get; set; }
+        public string GNRL_DESCR_02 { get; set; }
+        public string GNRL_DESCR_03 { get; set; }
+        public string GNRL_DESCR_04 { get; set; }
+        public string GNRL_DESCR_05 { get; set; }
+        public string GNRL_DESCR_06 { get; set; }
+        public string GNRL_DESCR_07 { get; set; }
+        public string GNRL_DESCR_08 { get; set; }
+        public string GNRL_DESCR_09 { get; set; }
+        public string GNRL_DESCR_10 { get; set; }
+        public string GNRL_DESCR_11 { get; set; }
+        public string GNRL_DESCR_12 { get; set; }
+        public string GNRL_DESCR_13 { get; set; }
+        public string GNRL_DESCR_14 { get; set; }
+        public string GNRL_DESCR_15 { get; set; }
+        public string GNRL_DESCR_16 { get; set; }
+        public string GNRL_DESCR_17 { get; set; }
+        public string GNRL_DESCR_18 { get; set; }
+        public string GNRL_DESCR_19 { get; set; }
+        public string GNRL_DESCR_20 { get; set; }
+        public string GNRL_DESCR_21 { get; set; }
+        public string GNRL_DESCR_22 { get; set; }
+        public string GNRL_DESCR_23 { get; set; }
+        public string GNRL_DESCR_24 { get; set; }
+        public string GNRL_DESCR_25 { get; set; }
+        public DateTime? GNRL_1_DATETIME { get; set; }
+        public DateTime? GNRL_2_DATETIME { get; set; }
+        public DateTime? GNRL_3_DATETIME { get; set; }
+        public DateTime? GNRL_4_DATETIME { get; set; }
+        public DateTime? GNRL_5_DATETIME { get; set; }
+        public string LINK_REF_1 { get; set; }
     }
 }
