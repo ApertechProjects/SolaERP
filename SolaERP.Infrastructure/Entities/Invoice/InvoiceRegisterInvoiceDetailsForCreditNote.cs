9e02c1516e67a331f725c7787080e4770d46c950
Subhan Masimov <subhanmasimov02@gmail.com>
2025-06-17 15:52:12 +0400

    add analysis codes

--- SolaERP.Infrastructure/Entities/Invoice/InvoiceRegisterInvoiceDetailsForCreditNote.cs
+++ SolaERP.Infrastructure/Entities/Invoice/InvoiceRegisterInvoiceDetailsForCreditNote.cs
@@ -29,15 +29,15 @@ public class InvoiceRegisterInvoiceDetailsForCreditNote : BaseEntity
     public string ANAL_T6 { get; set; }
     public string ANAL_T7 { get; set; }
     public string ANAL_T8 { get; set; }
-    public int AnalysisCode1Id { get; set; }
-    public int AnalysisCode2Id { get; set; }
-    public int AnalysisCode3Id { get; set; }
-    public int AnalysisCode4Id { get; set; }
-    public int AnalysisCode5Id { get; set; }
-    public int AnalysisCode6Id { get; set; }
-    public int AnalysisCode7Id { get; set; }
-    public int AnalysisCode8Id { get; set; }
-    public int AnalysisCode9Id { get; set; }
-    public int AnalysisCode10Id { get; set; }
+    public int? AnalysisCode1Id { get; set; }
+    public int? AnalysisCode2Id { get; set; }
+    public int? AnalysisCode3Id { get; set; }
+    public int? AnalysisCode4Id { get; set; }
+    public int? AnalysisCode5Id { get; set; }
+    public int? AnalysisCode6Id { get; set; }
+    public int? AnalysisCode7Id { get; set; }
+    public int? AnalysisCode8Id { get; set; }
+    public int? AnalysisCode9Id { get; set; }
+    public int? AnalysisCode10Id { get; set; }
    
 }
