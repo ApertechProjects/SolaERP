a9f429ef4a60579d4eeec2f62c1e12620a6c186f
Subhan Masimov <subhanmasimov02@gmail.com>
2025-06-23 18:12:04 +0400

    fix

--- SolaERP.DataAccess/DataAccess/SqlServer/SqlBusinessUnitRepository.cs
+++ SolaERP.DataAccess/DataAccess/SqlServer/SqlBusinessUnitRepository.cs
@@ -71,7 +71,7 @@ namespace SolaERP.DataAccess.DataAccess.SqlServer
         {
             using (var command = _unitOfWork.CreateCommand() as DbCommand)
             {
-                command.CommandText = "Select * from dbo.VW_BusinessUnits_List";
+                command.CommandText = "SELECT * FROM Config.BusinessUnits";
                 using var reader = await command.ExecuteReaderAsync();
 
                 List<BusinessUnits> businessUnits = new List<BusinessUnits>();
