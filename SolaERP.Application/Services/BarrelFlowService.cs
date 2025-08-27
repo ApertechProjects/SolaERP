a3a1bed8e123e4feb674747004ccabdbd3523309
anar_ceferov <anarceferov1996@gmail.com>
2025-08-27 16:59:03 +0400

    all procedure

--- SolaERP.Application/Services/BarrelFlowService.cs
+++ SolaERP.Application/Services/BarrelFlowService.cs
@@ -34,10 +34,85 @@ public class BarrelFlowService : IBarrelFlowService
         return ApiResponse<bool>.Success(true, 200);
     }
 
-    public async Task<ApiResponse<List<BarrelFlowRegisterDto>>> BarrelFlowRegisterAsync(int businessUnitId,
+    public async Task<ApiResponse<List<BarrelFlowRegisterDto>>> GetBarrelFlowRegisterAsync(int businessUnitId,
         DateTime dateFrom, DateTime dateTo)
     {
         var result = await _barrelFlowRepository.GetBarrelFlowRegister(businessUnitId, dateFrom, dateTo);
         return ApiResponse<List<BarrelFlowRegisterDto>>.Success(result, 200);
+    }
+
+    public async Task<ApiResponse<List<BarrelFlowPeriodConvertListDto>>> GetPeriodListByBusinessIdAsync(
+        int businessUnitId)
+    {
+        var data = await _barrelFlowRepository.GetPeriodListByBusinessId(businessUnitId);
+
+        List<BarrelFlowPeriodConvertListDto> result = new List<BarrelFlowPeriodConvertListDto>();
+
+        foreach (var d in data)
+        {
+            var periodFrom = Int32.Parse(d.PeriodFrom);
+            var periodTo = Int32.Parse(d.PeriodTo);
+
+            for (int i = periodFrom; i <= periodTo; i++)
+            {
+                if (i % 100 == 13)
+                {
+                    var nextYear = ((i / 1000) + 1);
+                    // Novbeti il 2024012
+                    i = Int32.Parse(nextYear.ToString() + "000");
+                    continue;
+                }
+
+                BarrelFlowPeriodConvertListDto dto = new BarrelFlowPeriodConvertListDto();
+                dto.name = i.ToString();
+                result.Add(dto);
+            }
+        }
+
+        return ApiResponse<List<BarrelFlowPeriodConvertListDto>>.Success(result, 200);
+    }
+
+    public async Task<ApiResponse<bool>> SaveBarrelFlowBudgetRegisterAsync(List<BarrelFlowBudgetUIDDto> data)
+    {
+        var table = data.ConvertListOfCLassToDataTable();
+        var result = await _barrelFlowRepository.SaveBarrelFlowBudgetRegisterIUD(table);
+        await _unitOfWork.SaveChangesAsync();
+        return ApiResponse<bool>.Success(true, 200);
+    }
+
+    public async Task<ApiResponse<List<BarrelFlowBudgetRegisterDto>>> GetBarrelFlowBudgetRegisterAsync(
+        int businessUnitId,
+        DateTime dateFrom, DateTime dateTo)
+    {
+        var result = await _barrelFlowRepository.GetBarrelFlowBudgetRegister(businessUnitId, dateFrom, dateTo);
+        return ApiResponse<List<BarrelFlowBudgetRegisterDto>>.Success(result, 200);
+    }
+
+    public async Task<ApiResponse<bool>> SaveProductionRevenueRegisterAsync(List<ProductionRevenueRegisterIUDDto> data)
+    {
+        var table = data.ConvertListOfCLassToDataTable();
+        var result = await _barrelFlowRepository.SaveProductionRevenueRegisterIUD(table);
+        await _unitOfWork.SaveChangesAsync();
+        return ApiResponse<bool>.Success(true, 200);
+    }
+
+    public async Task<ApiResponse<List<ProductionRevenueListDto>>> GetProductionRevenueRegisterAsync(
+        int businessUnitId,
+        DateTime dateFrom, DateTime dateTo)
+    {
+        var result = await _barrelFlowRepository.GetProductionRevenueRegister(businessUnitId, dateFrom, dateTo);
+        return ApiResponse<List<ProductionRevenueListDto>>.Success(result, 200);
+    }
+
+    public async Task<ApiResponse<List<FactForecastListDto>>> GetFactForecastListAsync()
+    {
+        var result = await _barrelFlowRepository.GetFactForecastList();
+        return ApiResponse<List<FactForecastListDto>>.Success(result, 200);
+    }
+
+    public async Task<ApiResponse<List<QuarterListDto>>> GetQuarterListAsync()
+    {
+        var result = await _barrelFlowRepository.GetQuarterList();
+        return ApiResponse<List<QuarterListDto>>.Success(result, 200);
     }
 }
---
+++ SolaERP.Infrastructure/Dtos/BarrelFlow/FactForecastListDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SolaERP.Application.Dtos.BarrelFlow;
+
+public class FactForecastListDto
+{
+    public int FactForecastId { get; set; }
+    public string Name { get; set; }
+}
