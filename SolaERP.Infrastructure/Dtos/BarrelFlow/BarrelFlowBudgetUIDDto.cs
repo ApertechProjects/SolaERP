996b207dfd8b396346def4805091f0521fd899e0
anar_ceferov <anarceferov1996@gmail.com>
2025-08-28 14:12:05 +0400

    bugfix

--- SolaERP.Infrastructure/Dtos/BarrelFlow/BarrelFlowBudgetUIDDto.cs
+++ SolaERP.Infrastructure/Dtos/BarrelFlow/BarrelFlowBudgetUIDDto.cs
@@ -2,18 +2,18 @@
 
 public class BarrelFlowBudgetUIDDto
 {
-    public int BarrelFlowBudgetRegisterId { get; set; }
-    public int Period { get; set; }
-    public DateTime Date { get; set; }
-    public decimal ProductionGross { get; set; }
-    public decimal LossPercent { get; set; }
-    public decimal ProductionNet { get; set; }
-    public decimal Delivery { get; set; }
-    public decimal ExportOilPercent { get; set; }
-    public decimal ExportOil { get; set; }
-    public decimal COPercent { get; set; }
-    public decimal CO { get; set; }
-    public decimal BarrelFactor { get; set; }
-    public int UserId { get; set; }
-    public int BusinessUnitId { get; set; }
+    public int? BarrelFlowBudgetRegisterId { get; set; }
+    public int? Period { get; set; }
+    public DateTime? Date { get; set; }
+    public decimal? ProductionGross { get; set; }
+    public decimal? LossPercent { get; set; }
+    public decimal? ProductionNet { get; set; }
+    public decimal? Delivery { get; set; }
+    public decimal? ExportOilPercent { get; set; }
+    public decimal? ExportOil { get; set; }
+    public decimal? COPercent { get; set; }
+    public decimal? CO { get; set; }
+    public decimal? BarrelFactor { get; set; }
+    public int? UserId { get; set; }
+    public int? BusinessUnitId { get; set; }
 }
