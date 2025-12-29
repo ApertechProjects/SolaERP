a02b58badfe285eaece2483f299214d68da0b630
unknown <iqbalqasimov4@gmail.com>
2025-12-29 10:18:44 +0400

    transactionDate added to InvoiceRegisterListByOrder api
    # Conflicts:
    #	SolaERP.Infrastructure/Dtos/Invoice/RegisterListByOrderDto.cs
    #	SolaERP.Infrastructure/Entities/Invoice/RegisterListByOrder.cs

--- SolaERP.Infrastructure/Dtos/Invoice/RegisterListByOrderDto.cs
+++ SolaERP.Infrastructure/Dtos/Invoice/RegisterListByOrderDto.cs
@@ -8,9 +8,6 @@
         public DateTime InvoiceReceivedDate { get; set; }
         public string InvoiceComment { get; set; }
         public decimal OrderTotal { get; set; }
-        public int? TaxId { get; set; }
-        public int? InvoicePeriod { get; set; }
-        public bool WHTGrossUp { get; set; }
-        public int? WithHoldingTaxId { get; set; }
+        // public string WithHoldingTaxCode { get; set; }
     }
 }
